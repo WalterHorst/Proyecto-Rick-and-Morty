5a3c5ff03307bf1eef310f4d1577dfe8ee91648d
WalterHorst <whorst42@gmail.com>
2023-06-28 14:06:55 -0300

    .gitignore

--- rick-and-morty/server/src/index.js
+++ rick-and-morty/server/src/index.js
@@ -1,16 +1,19 @@
 const http = require("http");
-const data = require("./utils/data")
+const data = require("./utils/data");
 
-http.createServer((request, response)=>{
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    if(request.url.includes("/rickandmorty/character")){
-        let id = request.url.split("/").at(-1)
-        
-        const characterFound = data.find((character)=>{ return character.id===Number(id)})
-    
-        response
-        .writeHead(200, {"Conten-type":"aplication/json"})
-        .end(JSON.stringify(characterFound))
-    }   
-})
-.listen(3001)
+http
+  .createServer((request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    if (request.url.includes("/rickandmorty/character")) {
+      let id = request.url.split("/").at(-1);
+
+      const characterFound = data.find((character) => {
+        return character.id === Number(id);
+      });
+
+      response
+        .writeHead(200, { "Conten-type": "aplication/json" })
+        .end(JSON.stringify(characterFound));
+    }
+  })
+  .listen(3001);
